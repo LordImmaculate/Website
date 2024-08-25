63f82846607f67c7fd3376ecad83219fce3b3a5c
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2024-08-25 18:38:00 +0200

    Remove Turnstile

---
+++ script/email.js
@@ -0,0 +1,8 @@
+function showEmailPopup() {
+    document.getElementById('emailPopup').classList.add('show');
+    console.log('showEmailPopup');
+}
+
+function hideEmailPopup() {
+    document.getElementById('emailPopup').classList.remove('show');
+}
