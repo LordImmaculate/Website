86f9f11f14df2ab0c0f00f86c42dafda9d4ede7e
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2024-08-29 20:06:42 +0200

    Improved email popup

--- script/cf-turnstile.js
+++ script/cf-turnstile.js
@@ -1,4 +1,6 @@
 function onTurnstileSuccess(token) {
             document.getElementById('emailLink').classList.remove('hidden');
+            document.getElementById('emailText').classList.remove('hidden');
             document.getElementById('cf-turnstile').classList.add('hidden');
+            document.getElementById('emailTitle').innerHTML = 'Send me an email';
         }
